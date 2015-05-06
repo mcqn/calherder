cb0a11a8997f90f8b027d2cdbc625ac9e1a607c3
Adrian McEwen <adrianm@mcqn.com>
2015-05-06 23:22:54 +0100

    Removed unnecessary commented out code

--- cal_filter.rb
+++ cal_filter.rb
@@ -9,18 +9,6 @@ require 'net/http'
 require 'net/https'
 require 'ri_cal'
 #require 'time_start_and_end_extensions'
-require './local_config'
-
-# Download and parse the calendar
-#if (CAL_URL_IS_HTTPS)
-#  cal_uri = URI.parse(CAL_URL)
-#  cal_http = Net::HTTP.new(cal_uri.host, 443)
-#  cal_http.use_ssl = true
-#  cal_req = Net::HTTP::Get.new(cal_uri.request_uri)
-#  cal_data = cal_http.request(cal_req)
-#else
-#  cal_data = Net::HTTP.get_response(URI.parse(CAL_URL))
-#end
 
 # Grab the data from stdin
 # FIXME Use ARGV to read in parameters to filter first
